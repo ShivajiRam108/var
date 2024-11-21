a1aed7eeb281a8e1e4a37b6c7e81e15ca0fb15e8
ShivajiRam108-github <ramudus684@gmail.com>
2024-11-21 19:39:06 +0530

    var

---
+++ index.js
@@ -0,0 +1,217 @@
+//**********question  1   ******************
+//  var x;
+// x = 5;                 Error
+// var x = 10;
+
+
+
+//********    question  2       ************
+console.log(x);          undefined
+var x = 15; 
+
+
+
+// ************question  3  ************
+// var y=1; 
+// var y=2;                   error
+// y=3;             
+
+
+
+// ************question  4  ************
+// var b;                       undefined
+// console.log(b);
+
+
+
+
+// ************question  5  ************
+// var a = 1;
+// {
+//     var a = 2;           2
+// }
+// console.log(a);
+
+
+
+
+// ************question  6  ************
+// {
+//     var c = 10;
+// }                      10
+// console.log(c);
+
+
+
+
+// ************question  7  ************
+// var p;
+// console.log(p);    undefined
+// p = 20;
+
+
+
+// ************question  8  ************
+// var q = 5;  
+// var q = 10;                10
+// console.log(q);
+
+
+
+// ************question  9  ************
+// var r;
+// r = 30;                  error
+// var r = 50;
+
+
+
+
+// ************question  10  ************
+// console.log(x);
+// var x = 25;                undefined
+
+
+
+// ************question  11  ************
+// console.log(x);               undefined
+// var x = 25;
+
+
+
+// ************question  12  ************
+// var t = 10;  
+// var t;                             10
+// console.log(t);
+
+
+
+
+// ************question  13  ************
+// x = 100;  
+// var x;                         100
+// console.log(x);
+
+
+
+// ************question  14  ************
+// var y = 5;             error
+// var y = 10;  
+
+
+
+// ************question  15  ************
+// var z = 1;  
+// z = 2;                       2
+// console.log(z);
+
+
+
+// ************question  16  ************
+// var _Xyz12$;
+// _Xyz12$ = 45;                    45
+// console.log(_Xyz12$);
+
+
+
+// ************question  17  ************
+// var $num_Val = 8;  
+// $num_Val = 16;                   16
+// console.log($num_Val);
+
+
+
+// ************question  18  ************
+// var $num_Val = 8;  
+// $num_Val = 16;                       16
+// console.log($num_Val);
+
+
+
+// ************question  19  ************
+// var Val_12X$ = 10;  
+// Val_12X$ = 20;                         20
+// console.log(Val_12X$);
+
+
+
+// ************question  20  ************
+// console.log(VAL_num);  
+// var VAL_num = "Test";              Error
+
+
+
+// ************question  21  ************
+// var $count = 5;  
+// $count = $count + 3;                    8
+// console.log($count);
+
+
+
+// ************question  22  ************
+// var _alpha$ = 50;  
+// var _alpha$ = 100;                       100
+// console.log(_alpha$);
+
+
+
+// ************question  23  ************
+// var A1b_2$ = 1;  
+// console.log(A1b_2$);                       1
+
+
+
+
+// ************question  24  ************
+// var $$$ = 99;  
+// $$$ = 88;                                 88
+// console.log($$$);
+
+
+
+
+// ************question  25  ************
+// var _vA_rX = "Code";  
+// console.log(_vA_rX);                    code
+
+
+
+// ************question  26  ************
+// var X_$12;  
+// console.log(X_$12);                  undefined
+// X_$12 = 123;
+
+
+
+// ************question  27  ************
+// var __X_var$ = "Hello";  
+// var __X_var$ = "World";                    world
+// console.log(__X_var$);
+
+
+
+// ************question  28  ************
+// var VaR_$$ = "Testing";  
+// console.log(VaR_$$);                     testing
+
+
+
+
+// ************question  29  ************
+// console.log($$$Alpha);                   undefined
+// var $$$Alpha = 55;  
+
+
+
+
+// ************question  30  ************
+// var 12x$var_ = 100;                    SyntaxError
+// console.log(12x$var_);
+
+
+
+
+
+
+
+
+
